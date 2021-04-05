3d901bd07a13312da81a8cad7c16c74e45233bcb
Kuruto Denzeru <kurtdenzel51@gmail.com>
2021-04-05 22:24:34 +0800

    Feet Conversion
    Feet Inches to Centimeters

--- ConversionFeet.java
+++ ConversionFeet.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class ConversionFeet {
     static Scanner console = new Scanner(System.in);
     public static void main (String [] args){
+        
         System.out.print("Enter Feet: ");
         double Feet=console.nextInt();  
+        
         System.out.print("Enter Inches: ");
         double Inch=console.nextInt();
         
         double SumInch = Feet * 12 + Inch;
         double SumCenti = SumInch * 2.54;
+        
+        //Output 
         System.out.println("The Number you entered are " + Feet  + " for Feet and " + Inch + "for Inches.");
         System.out.println("The Total number of inches: " + SumInch);
         System.out.println("The Total number of centimeters: " + SumCenti);
