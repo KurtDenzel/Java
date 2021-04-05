c9f20f5c6aa29cb081bce5f889235e3f389d2d10
Kuruto Denzeru <kurtdenzel51@gmail.com>
2021-04-05 23:08:32 +0800

    Multiplication Table
    Looping Condition to display a Multiplication Table

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+package Looping;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+    int a;
+    int b;
+        for (a = 1; a <= 9; ++a) {
+            for (b = 1; b <= 9; ++b) {
+            System.out.printf("%4d", (a*b)); //Multiplies
+            }
+            System.out.println();
+        }
+   }
+}
