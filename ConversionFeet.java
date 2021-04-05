f12775faeeed5f150e8c23383aba2d8f8b8cbb1c
Kuruto Denzeru <kurtdenzel51@gmail.com>
2021-04-05 15:19:07 +0800

    Data Methods
    Import Scanner

---
+++ ConversionFeet.java
@@ -0,0 +1,17 @@
+package JavaData;
+import java.util.*;
+public class ConversionFeet {
+    static Scanner console = new Scanner(System.in);
+    public static void main (String [] args){
+        System.out.print("Enter Feet: ");
+        double Feet=console.nextInt();  
+        System.out.print("Enter Inches: ");
+        double Inch=console.nextInt();
+        
+        double SumInch = Feet * 12 + Inch;
+        double SumCenti = SumInch * 2.54;
+        System.out.println("The Number you entered are " + Feet  + " for Feet and " + Inch + "for Inches.");
+        System.out.println("The Total number of inches: " + SumInch);
+        System.out.println("The Total number of centimeters: " + SumCenti);
+    }
+}
---
+++ UserGetInfo.java
@@ -0,0 +1,38 @@
+package JavaData;
+import java.util.*;
+public class UserGetInfo {
+    
+    public static void main(String[] args) {
+    //Declaration of Variables
+        String name;
+        float studentno;
+        String course;
+        int units;
+        float tuition;
+    
+        Scanner Index = new Scanner (System.in); //Scanner, Reads input
+        //Input Process
+        System.out.println("Please enter your Name: ");
+        name = Index.nextLine(); //Var Name as String Input
+    
+        System.out.println("Please enter your Student No.: ");
+        studentno = Index.nextFloat(); //Var studentno as Integer Input 
+    
+        System.out.println("Please enter your Course: ");
+        course = Index.next(); //Var course as String Input
+    
+        System.out.println("Please enter your Total Units Enrolled: ");
+        units = Index.nextInt();
+    
+        System.out.println("Please enter your Tuition: ");
+        tuition = Index.nextFloat(); //Var tuition as Interger Input
+
+        //Outputs of the Program
+        System.out.println("\nYour name is: " + name);
+        System.out.println("Student Number is: " + studentno);
+        System.out.println("Your Course is: " + course);
+        System.out.println("Your Total no. of units enrolled: " + units);
+        System.out.println("Total amount of tuition fee: " + tuition);
+    }
+    
+}
